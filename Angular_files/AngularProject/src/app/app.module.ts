79f9ad0a438a0ef14f3411ef01be03263655b9aa
RamTacenda <kenanaljedi@gmail.com>
2024-07-30 17:09:51 +0530

    The changes has been made

--- Angular_files/AngularProject/src/app/app.module.ts
+++ Angular_files/AngularProject/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChildcomponentComponent } from './childcomponent/childcomponent.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildcomponentComponent
   ],
   imports: [
     BrowserModule,
